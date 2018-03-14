1bc9c7ac361a3a697cd451470923d39d911f573d
Karítas Sif Halldórsdóttir <jovin38@gmail.com>
2018-03-14 20:39:22 +0000

    Add files via upload
    Vefþjón í node

---
+++ veftjon_hugbo2/models/question.js
@@ -0,0 +1,11 @@
+var mongoose=require('mongoose');
+var Schema=mongoose.Schema;
+
+var questionSchema = new Schema({
+  title: String,
+  releaseYear: String,
+  director: String,
+  genre: String
+});
+
+module.exports = mongoose.model('Question', questionSchema);
